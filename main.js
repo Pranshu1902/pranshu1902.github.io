410417abd1b2fed3eef44135f220c4312b045477
Pranshu1902 <pranshu1902@gmail.com>
2022-07-07 23:30:43 +0530

    updated theme

--- main.js
+++ main.js
@@ -1,86 +1,122 @@
-var c = document.getElementById("bounce");
-let ctx = c.getContext("2d");
+let projects = [
+  {
+    title: "Money Manager",
+    description:
+      "Ever felt like spending a lot of money without keeping any track? Use our site to manage all your money and know exactly where it is going.",
+    link: "https://money-manager-pranshu1902.netlify.app/",
+    repo: "https://github.com/Pranshu1902/Money-Manager-fe",
+    image: "projects/money_manager.png",
+  },
+  {
+    title: "P-Chat",
+    description:
+      "Chat like never before. A free and open-source chatting site to connect with your friends. Built using Django. Fast, secure, reliable, easy to use.",
+    link: "http://p-chat-pranshu1902.herokuapp.com/",
+    repo: "https://github.com/Pranshu1902/P-Chat/",
+    image: "projects/p_chat.png",
+  },
+  {
+    title: "The Gladden Project",
+    description:
+      "Feel like you want some personalized advice for your mental stress but are afraid to share you personal info with stranger? Then use your platform where you can get personalized guidance from our bot without worrying about privacy.",
+    link: "http://pranshu1902.github.io/TheGladdenProject/",
+    repo: "https://github.com/Pranshu1902/TheGladdenProject/",
+    image: "projects/gladdenproject.png",
+  },
+  {
+    title: "Task Manager",
+    description:
+      "Ever felt like you have a lot of tasks and not able to keep record of all of them? Well maybe you write down the tasks on a paper but to put them with priority and update them constantly can be hectic on paper. That's why I made this.",
+    link: "https://pranshu19-task-manager.herokuapp.com/",
+    repo: "https://github.com/Pranshu1902/WD-202-Task-Manager/",
+    image: "projects/task_manager.png",
+  },
+  {
+    title: "Martial Arts Simulator",
+    description:
+      "This is an online game where I have simulted a martial artist with some commands like kick, punch, block, etc.",
+    link: "/Martial%20arts/index.html",
+    repo: "https://github.com/Pranshu1902/pranshu1902.github.io/tree/master/Martial arts",
+    image: "projects/martial arts.png",
+  },
+  {
+    title: "Jarvis: Meeting Manager",
+    description:
+      "Automatically joins meetings for me based on the time table without any commands. Also, it acts as a virtual assistant and helps me with day-to-day tasks.",
+    link: "https://drive.google.com/u/0/uc?id=1ewjKZQvJko5kxUpUTeKQ5z1BGsah-34l&export=download",
+    repo: "https://github.com/Pranshu1902/Voice-Assistant-Jarvis",
+    image: "projects/jarvis.png",
+  },
+  {
+    title: "Breakout: Sixth Sense",
+    description:
+      "Forget keyboards, use your hand gestures to play the game of breakout.",
+    link: "https://pranshu1902.itch.io/breakout-sixth-sense-game",
+    repo: "https://github.com/Pranshu1902/Breakout-Sixth-sense-version",
+    image: "projects/breakout.png",
+  },
+  {
+    title: "Maze Solver AI",
+    description:
+      "This AI solves the maze using Depth-First Search and Breadth-First Search and returns an image comparing both algorithms. I have also made a GUI for this.",
+    repo: "https://github.com/Pranshu1902/Maze-Solver",
+    image: "projects/maze.png",
+  },
+  {
+    title: "ATM",
+    description: "Built a GUI based ATM which stores data in a csv file.",
+    repo: "https://github.com/Pranshu1902/ATM",
+    image: "projects/atm.png",
+  },
+];
 
-var c1 = document.getElementById("bounce2");
-let ctx1 = c1.getContext("2d");
+let div = document.getElementById("projects");
 
-var c2 = document.getElementById("bounce3");
-let ctx2 = c2.getContext("2d");
+let mainCode = "";
+let divClass = "";
 
-// creating the image elements
-var Img = document.createElement("img");
-Img.src = "github1.svg";
+let projectLink = "";
 
-var Img1 = document.createElement("img");
-Img1.src = "linkedin.png";
+projects.forEach((project, index) => {
+  index % 2 == 0
+    ? (divClass = "flex flex-col lg:flex-row gap-4")
+    : (divClass = "flex flex-col lg:flex-row-reverse gap-4");
 
-var Img2 = document.createElement("img");
-Img2.src = "twitter.png";
+  project.link
+    ? (projectLink = `<a
+  href="${project.link}"
+  target="_blank"
+>
+  <button class="font-bold border-2 btn rounded-lg px-4 py-1" type="button">
+    Checkout
+  </button>
+</a>`)
+    : (projectLink = "");
 
-// creating circlular background around image
-let drawCircle = (centerx, centery, radius, color, ctx) => {
-  ctx.beginPath();
-  ctx.arc(centerx, centery, radius, 0, 2 * Math.PI);
-  ctx.fillStyle = color;
-  ctx.fill();
-  ctx.stroke();
-};
+  mainCode += `
+  <div class="${divClass}">
+    <div class="w-1/2">
+      <img
+        src="${project.image}"
+        class="rounded-lg"
+        alt="${project.title}"
+        width="100%"
+        height="50%"
+      />
+    </div>
+    <div class="w-1/2">
+      <p class="pl-10 pb-12 font-medium">
+        <b class="footerDark text-5xl">${project.title}</b><br /><br />${project.description}
+        <br /><br />
+        ${projectLink}
+        <a class="pl-4" href=${project.repo} target="_blank">
+          <button class="font-bold border-2 btn rounded-lg px-4 py-1" type="button">
+            Code
+          </button>
+        </a>
+      </p>
+    </div>
+  </div>`;
+});
 
-let y_icon = 110;
-let y_icon1 = 120;
-let stepY = 1;
-let x_icon = 40;
-let y_icon_linkedin = 130;
-let y_end = 130;
-canvas_x = 300;
-canvas_y = 300;
-let radius = 110;
-
-let y_icon_bg = (150 + 2 * y_icon) / 2;
-let y_icon_bg1 = (150 + 2 * y_icon1) / 2;
-let y_icon_bg2 = (150 + 2 * y_icon_linkedin) / 2;
-
-function myAnimation1() {
-  ctx.clearRect(0, 0, canvas_x, canvas_y);
-  if (y_icon_bg < radius || y_icon_bg > y_end) {
-    stepY = -stepY;
-  }
-  y_icon += stepY;
-  y_icon_bg += stepY;
-  drawCircle((2 * x_icon + 150) / 2, y_icon_bg, radius, "gray", ctx);
-  ctx.drawImage(Img, x_icon, y_icon, 150, 150);
-}
-
-function myAnimation2() {
-  ctx1.clearRect(0, 0, canvas_x, canvas_y);
-  if (y_icon_linkedin < radius || y_icon_linkedin > y_end) {
-    stepY = -stepY;
-  }
-  y_icon_linkedin += stepY;
-  y_icon_bg2 += stepY;
-  drawCircle((2 * x_icon + 150) / 2, y_icon_bg2, radius, "gray", ctx1);
-  ctx1.drawImage(Img1, x_icon, y_icon_linkedin, 150, 150);
-}
-
-function myAnimation3() {
-  ctx2.clearRect(0, 0, canvas_x, canvas_y);
-  if (y_icon1 < radius || y_icon1 > y_end) {
-    stepY = -stepY;
-  }
-  y_icon1 += stepY;
-  y_icon_bg1 += stepY;
-  drawCircle((2 * x_icon + 150) / 2, y_icon_bg1, radius, "gray", ctx2);
-  ctx2.drawImage(Img2, x_icon, y_icon1, 150, 150);
-}
-
-setInterval(() => {
-  myAnimation1();
-}, 50);
-
-setInterval(() => {
-  myAnimation2();
-}, 50);
-
-setInterval(() => {
-  myAnimation3();
-}, 50);
+div.innerHTML = mainCode;
